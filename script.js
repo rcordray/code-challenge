7be6f30b7980ea0e0575f90623c5abd31ecc850a
Robert Cordray <robertcordray@gmail.com>
2017-07-31 14:40:15 -0500

    not able to append div to DOM

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 console.log('javascript has been sourced');
 
+//generate button created
+
 $(document).ready(function() {
     $('#generate').on('click', function() {
         console.log('generate button was clicked');
-
+        $('body').append('<div id="newDiv"></div>');
     })
 
 
