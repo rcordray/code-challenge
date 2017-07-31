35341798e5853dda8e3834c95d7734d273182ef8
Robert Cordray <robertcordray@gmail.com>
2017-07-31 14:10:05 -0500

    created generate button with a click function

---
+++ script.js
@@ -0,0 +1,12 @@
+console.log('javascript has been sourced');
+
+$(document).ready(function() {
+    $('#generate').on('click', function() {
+        console.log('generate button was clicked');
+
+    })
+
+
+
+
+});
